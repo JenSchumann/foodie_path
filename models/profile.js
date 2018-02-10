7a035c914a0a0562fe1e6c1d777888f0f0e2d695
Jen Schumann <jenniferschumann1@gmail.com>
2018-02-10 12:58:43 -0700

    example profile page is created and possible avatar choices for users added

--- models/profile.js
+++ models/profile.js
@@ -5,6 +5,8 @@ const profileSchema = mongoose.Schema({
   author: {type:String, default: ''},
   email: { type: String, required: true, unique: true },
   avatar: { type: String, default: '' },
+  alias: { type: String, default: '' },
+  headline: { type: String, default: '' },
   bio: { type: String, default: '' },
 
   files: { type: String, default: '' },
