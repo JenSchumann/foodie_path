57fe18c34b8230ceb45de5869846a047411a82c9
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2018-02-14 12:28:18 -0700

    login and sessions working

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user.js');
+const User = require('../models/users.js');
 
 router.get('/new', (req, res) => {
   res.render('users/new.ejs');
 });
 
 router.post('/', (req, res) => {
+  req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
   User.create(req.body, function(err, createdUser){
     res.redirect('/');
   });
